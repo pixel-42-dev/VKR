1e65a3383ea3c7bb302890cfc355d83512605cba
Vadim Tkachev <vadim.tkachev.42@gmail.com>
2024-06-06 10:29:47 +0300

    fix

--- resources/views/master/master.blade.php
+++ resources/views/master/master.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="{{ asset('css/vendor.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
-    <title>Homepage - A</title>
+    <title>FashionHub</title>
   </head>
   <body>
 
